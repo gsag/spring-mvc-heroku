35a4fe29b0779ead20446d50f2f01f8387ed2f61
gsag <gsag.dh@gmail.com>
2015-11-20 12:44:58 -0200

    Criando script de deploy e arquivo de propriedades.

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+echo "==============================="
+echo "Fazendo deploy do projeto..."
+echo "==============================="
+echo ""
+
+heroku login
+#Descomente se ainda não criou um app no heroku
+#heroku create spring-mvc-3120
+git push heroku master
