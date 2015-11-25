ee2807f7e332caf105c06cbf3e439547f00f2324
gsag <gsag.dh@gmail.com>
2015-11-25 00:45:34 -0200

    Alterando datasource de produção.

--- src/main/java/config/ProductionDatabaseConfiguration.java
+++ src/main/java/config/ProductionDatabaseConfiguration.java
@@ -7,10 +7,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -31,19 +33,16 @@ public class ProductionDatabaseConfiguration{
 
     //    * Data Source Production
     @Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(env.getProperty("DATABASE_URL"));
+    public DataSource dataSource() throws URISyntaxException{
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        URI dbUrl = new URI(env.getProperty("DATABASE_URL"));
+        dataSource.setUrl("jdbc:postgresql://" + dbUrl.getHost() + ":" + dbUrl.getPort() + dbUrl.getPath());
+        dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
+        dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
 
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-
-        return basicDataSource;
+        return dataSource;
     }
 
     //    * Session Factory
