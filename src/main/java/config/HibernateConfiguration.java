dc0eb660999eade0bb207460ecfc3a4a98f64dac
gsag <gsag.dh@gmail.com>
2015-11-25 22:22:44 -0200

    Removendo dependência dbcp que se tornou desnecessária.

--- src/main/java/config/HibernateConfiguration.java
+++ src/main/java/config/HibernateConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Service;
 
 import java.util.Properties;
 
@@ -14,25 +13,25 @@ import java.util.Properties;
 @Configuration
 @PropertySource({"classpath:hibernate.properties"})
 public class HibernateConfiguration {
-    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
-    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
-    private static final String PROPERTY_NAME_HIBERNATE_FORMAT_SQL = "hibernate.format_sql";
-    private static final String PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
-    private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGE = "entitymanager.package";
+    private final String PROPERTY_HIBERNATE_DIALECT = "hibernate.dialect";
+    private final String PROPERTY_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
+    private final String PROPERTY_HIBERNATE_FORMAT_SQL = "hibernate.format_sql";
+    private final String PROPERTY_HIBERNATE_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
+    private final String PROPERTY_ENTITYMANAGER_PACKAGE = "entitymanager.package";
 
     @Autowired
     private Environment env;
 
     public Properties getHibernateProperties() {
         Properties properties = new Properties();
-        properties.setProperty(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO));
-        properties.setProperty(PROPERTY_NAME_HIBERNATE_DIALECT, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
-        properties.setProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
-        properties.setProperty(PROPERTY_NAME_HIBERNATE_FORMAT_SQL, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_FORMAT_SQL));
+        properties.setProperty(PROPERTY_HIBERNATE_HBM2DDL_AUTO, env.getRequiredProperty(PROPERTY_HIBERNATE_HBM2DDL_AUTO));
+        properties.setProperty(PROPERTY_HIBERNATE_DIALECT, env.getRequiredProperty(PROPERTY_HIBERNATE_DIALECT));
+        properties.setProperty(PROPERTY_HIBERNATE_SHOW_SQL, env.getRequiredProperty(PROPERTY_HIBERNATE_SHOW_SQL));
+        properties.setProperty(PROPERTY_HIBERNATE_FORMAT_SQL, env.getRequiredProperty(PROPERTY_HIBERNATE_FORMAT_SQL));
         return properties;
     }
 
     public String getEntityManagerPackage(){
-        return PROPERTY_NAME_ENTITYMANAGER_PACKAGE;
+        return PROPERTY_ENTITYMANAGER_PACKAGE;
     }
 }
--- src/main/java/config/ProductionDatabaseConfiguration.java
+++ src/main/java/config/ProductionDatabaseConfiguration.java
@@ -1,6 +1,5 @@
 package config;
 
-import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
